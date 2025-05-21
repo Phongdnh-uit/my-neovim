17f888e1f93eb20290b9ccda0d2cad15d87a5082
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2025-05-21 11:55:21 +0700

    📦 build(server): add some config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -5,71 +5,63 @@ local workspace_dir = workspace_path .. project_name
 
 local status, jdtls = pcall(require, "jdtls")
 if not status then
-    return
+	return
 end
 
 local config = {
-    cmd = {
-        "java",
-        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-        "-Dosgi.bundles.defaultStartLevel=4",
-        "-Declipse.product=org.eclipse.jdt.ls.core.product",
-        "-Dlog.protocol=true",
-        "-Dlog.level=ALL",
-        "-Xmx1g",
-        "--add-modules=ALL-SYSTEM",
-        "--add-opens",
-        "java.base/java.util=ALL-UNNAMED",
-        "--add-opens",
-        "java.base/java.lang=ALL-UNNAMED",
-        "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-        "-jar",
-        vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-        "-configuration",
-        home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
-        "-data",
-        workspace_dir,
-    },
-    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+	cmd = {
+		"java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+		"-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+		"-jar",
+		vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+		"-configuration",
+		home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
+		"-data",
+		workspace_dir,
+	},
+	root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
 
-    settings = {
-        java = {
-            signatureHelp = { enabled = true },
-            maven = {
-                downloadSources = true,
-            },
-            referencesCodeLens = {
-                enabled = true,
-            },
-            references = {
-                includeDecompiledSources = true,
-            },
-            inlayHints = {
-                parameterNames = {
-                    enabled = "all",
-                },
-            },
-            format = {
-                enabled = false,
-            },
-            configuration = {
-                runtimes = {
-                    {
-                        name = "JavaSE-24",
-                        path = "/usr/java/jdk-24-oracle-x64",
-                    },
-                },
-            },
-        },
-    },
+	settings = {
+		java = {
+			signatureHelp = { enabled = true },
+			maven = {
+				downloadSources = true,
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			references = {
+				includeDecompiledSources = true,
+			},
+			inlayHints = {
+				parameterNames = {
+					enabled = "all",
+				},
+			},
+			format = {
+				enabled = false,
+			},
+		},
+	},
 
-    init_options = {
-        bundles = {
-            vim.fn.glob(
-                home .. "/.local/share/nvim/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin-*.jar",
-                1
-            ),
-        },
-    },
+	init_options = {
+		bundles = {
+			vim.fn.glob(
+				home .. "/.local/share/nvim/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin-*.jar",
+				1
+			),
+		},
+	},
 }
 require("jdtls").start_or_attach(config)
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,37 +1,37 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-    config = function()
-        vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {noremap = true, silent = true} )
-        require("neo-tree").setup({
-            close_if_last_window = true,
-            popup_border_style = "rounded",
-            enable_git_status = true,
-            default_component_configs = {
-                name = {
-                    trailing_slash = true,
-                    use_git_status_colors = true,
-                    highlight = "NeoTreeFileName",
-                },
-                git_status = {
-                symbols = {
-                  added = "+",
-                  modified = "",
-                  deleted = "✖",
-                  renamed = "󰁕",
-                  untracked = "",
-                  ignored = "",
-                  unstaged = "",
-                  staged = "",
-                  conflict = "",
-                }
-            }
-          },
-        })
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
+		require("neo-tree").setup({
+			close_if_last_window = false,
+			popup_border_style = "rounded",
+			enable_git_status = true,
+			default_component_configs = {
+				name = {
+					trailing_slash = true,
+					use_git_status_colors = true,
+					highlight = "NeoTreeFileName",
+				},
+				git_status = {
+					symbols = {
+						added = "+",
+						modified = "",
+						deleted = "✖",
+						renamed = "󰁕",
+						untracked = "",
+						ignored = "",
+						unstaged = "",
+						staged = "",
+						conflict = "",
+					},
+				},
+			},
+		})
+	end,
 }
