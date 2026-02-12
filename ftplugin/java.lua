06414e01efc568aab895d415d1ab0ba81dfe5c23
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-02-12 21:35:33 +0700

    🚧 build(server): add latex support

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,72 +1,95 @@
 local home = os.getenv("HOME")
 local workspace_path = home .. "/.local/share/nvim/jdtls-workspace/"
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace_dir = workspace_path .. project_name
+-- local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+-- local workspace_dir = workspace_path .. project_name
 
 local status, jdtls = pcall(require, "jdtls")
 if not status then
-    return
+	return
 end
 
+local root_markers = { "settings.gradle.kts", ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", "build.gradle.kts" }
+local root_dir = vim.fs.root(0, root_markers)
+
+if not root_dir then
+	return
+end
+
+local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
+local workspace_dir = workspace_path .. project_name
+
 local config = {
-    cmd = {
-        "java",
-        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-        "-Dosgi.bundles.defaultStartLevel=4",
-        "-Declipse.product=org.eclipse.jdt.ls.core.product",
-        "-Dlog.protocol=true",
-        "-Dlog.level=ALL",
-        "-Xmx1g",
-        "--add-modules=ALL-SYSTEM",
-        "--add-opens",
-        "java.base/java.util=ALL-UNNAMED",
-        "--add-opens",
-        "java.base/java.lang=ALL-UNNAMED",
-        "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-        "-jar",
-        vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-        "-configuration",
-        home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
-        "-data",
-        workspace_dir,
-    },
-    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+	cmd = {
+		"java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ERROR",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+		"-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+		"-jar",
+		vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+		"-configuration",
+		home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
+		"-data",
+		workspace_dir,
+	},
+	root_dir = root_dir,
 
-    settings = {
-        java = {
-            signatureHelp = { enabled = true },
-            maven = {
-                downloadSources = true,
-            },
-            referencesCodeLens = {
-                enabled = true,
-            },
-            references = {
-                includeDecompiledSources = true,
-            },
-            inlayHints = {
-                parameterNames = {
-                    enabled = "all",
-                },
-            },
-            format = {
-                enabled = false,
-            },
-        },
-    },
+	settings = {
+		java = {
+			import = {
+				gradle = {
+					enabled = true,
+				},
+				maven = {
+					enabled = true,
+				},
+			},
+			signatureHelp = { enabled = true },
+			maven = {
+				downloadSources = true,
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			references = {
+				includeDecompiledSources = true,
+			},
+			inlayHints = {
+				parameterNames = {
+					enabled = "all",
+				},
+			},
+			format = {
+				enabled = false,
+			},
+		},
+	},
 
-    init_options = {
-        bundles = {
-            vim.fn.glob(
-                home
-                .. "/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
-                1
-            ),
-        },
-    },
+	init_options = {
+		bundles = {
+			vim.fn.glob(
+				home
+					.. "/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
+				1
+			),
+		},
+	},
 }
 
 -- Add spring-boot jdtls extension jars
-vim.list_extend(config.init_options.bundles, require("spring_boot").java_extensions())
+local status_sb, spring_boot = pcall(require, "spring_boot")
+if status_sb then
+    vim.list_extend(config.init_options.bundles, spring_boot.java_extensions())
+end
+
+-- vim.list_extend(config.init_options.bundles, require("spring_boot").java_extensions())
 
 jdtls.start_or_attach(config)
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,13 +1,26 @@
 return {
-	"lervag/vimtex",
-	lazy = false, -- we don't want to lazy load VimTeX
-	-- tag = "v2.15", -- uncomment to pin to a specific release
-	init = function()
-		vim.cmd([[
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+        vim.cmd([[
           filetype plugin indent on
           syntax enable
     ]])
-		-- VimTeX configuration goes here, e.g.
-		vim.g.vimtex_view_method = "zathura"
-	end,
+        -- VimTeX configuration goes here, e.g.
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_compiler_method = "latexmk"
+        vim.g.vimtex_compiler_latexmk = {
+            build_dir = "",
+            callback = 1,
+            continuous = 1,
+            executable = "latexmk",
+            options = {
+                "-xelatex",
+                "-file-line-error",
+                "-synctex=1",
+                "-interaction=nonstopmode",
+            },
+        }
+    end,
 }
