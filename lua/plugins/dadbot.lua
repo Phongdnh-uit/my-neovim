b3ce2134d74f06921f0c4f05de7466ca5033cd5f
Phongdnh-uit <dangnguyenhuyphong@gmail.com>
2026-07-18 10:35:09 +0700

    upgrade dadbot-ui

--- lua/plugins/dadbot.lua
+++ lua/plugins/dadbot.lua
@@ -13,8 +13,12 @@ return {
 	init = function()
 		-- Your DBUI configuration
 		vim.g.db_ui_use_nerd_fonts = 1
+		vim.g.db_ui_show_database_icon = 1
 
-		vim.g.db_ui_sidebar_position = "right" -- Set the sidebar position to the right
+		vim.g.db_ui_win_position = "right"
+		vim.g.db_ui_winwidth = 40
+
+		vim.g.db_ui_use_nvim_notify = 1
 	end,
 }
 -- { -- optional saghen/blink.cmp completion source
